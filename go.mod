a0aa5963836042ecfa4a522974593a061ad184be
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-06-30 22:12:47 -0300

    add alice and logging middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/danilocordeirodev/go-kanban-api
 
 go 1.22.4
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
 )
 
 type RouteResponse struct {
 	Message string `json:"message"`
+	ID      string `json:"id,omitempty"`
 }
 
 func main() {
@@ -19,10 +21,24 @@ func main() {
 
 	log.Println("Setting up routes...")
 
-	router.HandleFunc("/").Methods("GET")
+	router.Handle("/register", alice.New(loggingMiddleware).ThenFunc(register)).Methods("POST")
+	router.Handle("/login", alice.New(loggingMiddleware).ThenFunc(login)).Methods("POST")
+	router.Handle("/projects", alice.New(loggingMiddleware).ThenFunc(createProject)).Methods("POST")
+	router.Handle("/projects/{id}", alice.New(loggingMiddleware).ThenFunc(updateProject)).Methods("PUT")
+	router.Handle("/projects", alice.New(loggingMiddleware).ThenFunc(getProjects)).Methods("GET")
+	router.Handle("/projects/{id}", alice.New(loggingMiddleware).ThenFunc(getProject)).Methods("GET")
+	router.Handle("/projects/{id}", alice.New(loggingMiddleware).ThenFunc(deleteProject)).Methods("DELETE")
 
 	log.Println("Listening on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+			next.ServeHTTP(w, r)
+		})
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
@@ -44,9 +60,11 @@ func createProject(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateProject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 	w.Header().Set("Content-type", "application/json")
 
-	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from updateProject"})
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from updateProject", ID: id})
 }
 
 func getProjects(w http.ResponseWriter, r *http.Request) {
@@ -56,13 +74,17 @@ func getProjects(w http.ResponseWriter, r *http.Request) {
 }
 
 func getProject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 	w.Header().Set("Content-type", "application/json")
 
-	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from getProject"})
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from getProject", ID: id})
 }
 
 func deleteProject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 	w.Header().Set("Content-type", "application/json")
 
-	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from deleteProject"})
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from deleteProject", ID: id})
 }
